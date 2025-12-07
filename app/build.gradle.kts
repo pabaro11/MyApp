7a916e855c5f290b746ecd556cc93ac606b30cfc
pabaro11 <pabaro8207@gmail.com>
2025-12-07 15:32:50 +0900

    "Initial commit"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil:2.4.0")
+
 }
